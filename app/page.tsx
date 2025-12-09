b9c5efd88c9cecbe3869ea98dbd3c9f3ab793363
WILLATRONIX <111201886+WILLATRONIX@users.noreply.github.com>
2025-12-09 23:56:41 +0000

    wip

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,7 @@
-import Image from 'next/image';
-import { Button } from '@/components/ui/button';
-
-
 export default function Home() {
 	return (
 		<div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-			<p>hello</p>
-			<Button variant="outline">Button</Button>
+			<p>Work In Progress (9/12/25)</p>
 		</div>
 	);
 }
