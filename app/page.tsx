ff1f70b4a894bef26c5a78c28d8571ce6ad2c39e
WILLATRONIX <111201886+WILLATRONIX@users.noreply.github.com>
2025-12-12 15:33:20 +0000

    sync

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,17 @@
+import { Button } from '@/components/ui/button';
+import Navbar from '@/components/blocks/navbar';
+
 export default function Home() {
 	return (
-		<div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-			<p>Work In Progress (9/12/25)</p>
+		<div>
+			<Navbar/>
+			<div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+				<p>Work In Progress (9/12/25)</p>
+				<Button variant="default">Button</Button>
+				<svg height="100" width="100" xmlns="http://www.w3.org/2000/svg">
+					<polygon points="10,0 20,20 0,20" style={{ fill: '#FFF' }}/>
+				</svg>
+			</div>
 		</div>
 	);
 }
---
+++ components/blocks/navbar.tsx
@@ -0,0 +1,9 @@
+import { Button } from '@/components/ui/button';
+
+export default function Navbar() {
+	return (
+		<div className="flex items-center justify-center bg-zinc-50 font-sans">
+			test
+		</div>
+	);
+}
